1c5be3552728967546f232b356134647ffd4b6fb
hash3liZer <sheikhshameerkashif@protonmail.com>
2020-09-29 10:19:26 +0530

    Basic Enumeration

--- subnest.py
+++ subnest.py
@@ -1,6 +1,7 @@
 import sys
 import os
 import re
+import json
 import argparse
 import requests
 from bs4 import BeautifulSoup as soup
@@ -49,8 +50,14 @@ class PULL:
     def start(self, mess=""):
         print(self.YELLOW + "[>] " + self.END + mess + self.END)
 
+    def tab(self, key, mess=""):
+        print(self.RED + " -  " + key + ": " + self.END + mess)
+
     def end(self, mess):
         print(self.GREEN + "[<] " + self.END + mess + self.END)
+
+    def error(self, mess=""):
+        print(self.RED + "[-] " + self.END + mess + self.END)
 
     def exit(self, mess=""):
         sys.exit(self.RED + "[~] " + self.END + mess + self.END)
@@ -59,8 +66,38 @@ pull = PULL()
 
 class RECON:
 
-    def __init__(self):
-        pass
+    GHEADERS = {
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:80.0) Gecko/20100101 Firefox/80.0',
+        'Accept': 'application/json, text/plain, */*',
+        'Accept-Language': 'en-US,en;q=0.5',
+        'Referer': 'https://otx.alienvault.com/',
+        'X-OTX-USM-USER': '0'
+    }
+    URL_GENERAL = "https://otx.alienvault.com/otxapi/indicator/domain/general/{domain}"
+
+    def __init__(self, prs):
+        self.domain = prs.domain
+        self.filter_all = prs.filter_all
+
+    def enum_basic(self):
+        url = self.URL_GENERAL.format(domain = self.domain)
+        pull.start("Requesting Basic Info!")
+        r = requests.get(url, headers=self.GHEADERS)
+
+        if r.status_code == 200:
+            data = json.loads(r.text)
+            pull.tab("Indicator", data["indicator"])
+            pull.tab("Alexa", data["alexa"])
+            pull.tab("Whois", data["whois"])
+            pull.tab("Pulse Count", data["pulse_info"]["count"])
+            if len(data["validation"]) and data["validation"][0]["source"] == "alexa":
+                pull.tab("Alexa Rank", data["validation"][0]["message"].split(":").strip(" "))
+            pull.tab("Sections", ", ".join(data["sections"]))
+        else:
+            pull.error("Error Requesting Basic Info RS [Invalid Code Received]")
+
+    def engage(self):
+        self.enum_basic()
 
 class PARSER:
 
@@ -68,6 +105,7 @@ class PARSER:
 
     def __init__(self, prs):
         self.domain = self.v_domain(prs.domain)
+        self.filter_all = prs.filter_all
 
     def v_domain(self, vl):
         if vl:
@@ -82,12 +120,14 @@ def main():
     parser = argparse.ArgumentParser()
 
     parser.add_argument('-d', '--domain', dest="domain", default="", type=str, help="Target Domain")
+    parser.add_argument('--all', dest="filter_all", default=False, action="store_true", help="Enumerate everything!")
 
     parser = parser.parse_args()
     parser = PARSER(parser)
 
     pull.start("Starting Recon Engine!")
-    
+    recon = RECON(parser)
+    recon.engage()
     pull.end("Done!")
 
 if __name__ == "__main__":
